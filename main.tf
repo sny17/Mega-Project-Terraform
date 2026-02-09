9183b593fecbbf4b61ceeec77c6a6ed6955d3bb1
sny17 <144407793+sny17@users.noreply.github.com>
2026-02-09 19:10:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,56 +1,57 @@
+
 provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "devopsshack_vpc" {
+resource "aws_vpc" "sathya_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "devopsshack-vpc"
+    Name = "sathya_vpc"
   }
 }
 
-resource "aws_subnet" "devopsshack_subnet" {
+resource "aws_subnet" "sathya_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.devopsshack_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.sathya_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.sathya_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "devopsshack-subnet-${count.index}"
+    Name = "sathya_subnetsubnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "devopsshack_igw" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_internet_gateway" "sathya_igw" {
+  vpc_id = aws_vpc.sathya_vpc.id
 
   tags = {
-    Name = "devopsshack-igw"
+    Name = "sathya-igw"
   }
 }
 
-resource "aws_route_table" "devopsshack_route_table" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_route_table" "sathya_route_table" {
+  vpc_id = aws_vpc.sathya_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.devopsshack_igw.id
+    gateway_id = aws_internet_gateway.sathya_igw.id
   }
 
   tags = {
-    Name = "devopsshack-route-table"
+    Name = "sathya-route-table"
   }
 }
 
-resource "aws_route_table_association" "devopsshack_association" {
+resource "aws_route_table_association" "sathya_association" {
   count          = 2
-  subnet_id      = aws_subnet.devopsshack_subnet[count.index].id
-  route_table_id = aws_route_table.devopsshack_route_table.id
+  subnet_id      = aws_subnet.sathya_subnet[count.index].id
+  route_table_id = aws_route_table.sathya_route_table.id
 }
 
-resource "aws_security_group" "devopsshack_cluster_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_security_group" "sathya_cluster_sg" {
+  vpc_id = aws_vpc.sathya_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +61,12 @@ resource "aws_security_group" "devopsshack_cluster_sg" {
   }
 
   tags = {
-    Name = "devopsshack-cluster-sg"
+    Name = "sathya-cluster-sg"
   }
 }
 
-resource "aws_security_group" "devopsshack_node_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_security_group" "sathya_node_sg" {
+  vpc_id = aws_vpc.sathya_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,23 +83,23 @@ resource "aws_security_group" "devopsshack_node_sg" {
   }
 
   tags = {
-    Name = "devopsshack-node-sg"
+    Name = "sathya-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "devopsshack" {
-  name     = "devopsshack-cluster"
-  role_arn = aws_iam_role.devopsshack_cluster_role.arn
+resource "aws_eks_cluster" "sathya" {
+  name     = "sathya-cluster"
+  role_arn = aws_iam_role.sathya_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.devopsshack_subnet[*].id
-    security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
+    subnet_ids         = aws_subnet.sathya_subnet[*].id
+    security_group_ids = [aws_security_group.sathya_cluster_sg.id]
   }
 }
 
 
 resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name    = aws_eks_cluster.devopsshack.name
+  cluster_name    = aws_eks_cluster.sathya.name
   addon_name      = "aws-ebs-csi-driver"
   
   resolve_conflicts_on_create = "OVERWRITE"
@@ -106,11 +107,11 @@ resource "aws_eks_addon" "ebs_csi_driver" {
 }
 
 
-resource "aws_eks_node_group" "devopsshack" {
-  cluster_name    = aws_eks_cluster.devopsshack.name
-  node_group_name = "devopsshack-node-group"
-  node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
-  subnet_ids      = aws_subnet.devopsshack_subnet[*].id
+resource "aws_eks_node_group" "sathya" {
+  cluster_name    = aws_eks_cluster.sathya.name
+  node_group_name = "sathya-node-group"
+  node_role_arn   = aws_iam_role.sathya_node_group_role.arn
+  subnet_ids      = aws_subnet.sathya_subnet[*].id
 
   scaling_config {
     desired_size = 3
@@ -122,12 +123,12 @@ resource "aws_eks_node_group" "devopsshack" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
+    source_security_group_ids = [aws_security_group.sathya_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "devopsshack_cluster_role" {
-  name = "devopsshack-cluster-role"
+resource "aws_iam_role" "sathya_cluster_role" {
+  name = "sathya-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -145,13 +146,13 @@ resource "aws_iam_role" "devopsshack_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy" {
-  role       = aws_iam_role.devopsshack_cluster_role.name
+resource "aws_iam_role_policy_attachment" "sathya_cluster_role_policy" {
+  role       = aws_iam_role.sathya_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "devopsshack_node_group_role" {
-  name = "devopsshack-node-group-role"
+resource "aws_iam_role" "sathya_node_group_role" {
+  name = "sathya-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -169,22 +170,22 @@ resource "aws_iam_role" "devopsshack_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_role_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "sathya_node_group_role_policy" {
+  role       = aws_iam_role.sathya_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_cni_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "sathya_node_group_cni_policy" {
+  role       = aws_iam_role.sathya_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_registry_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "sathya_node_group_registry_policy" {
+  role       = aws_iam_role.sathya_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_ebs_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "sathya_node_group_ebs_policy" {
+  role       = aws_iam_role.sathya_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
